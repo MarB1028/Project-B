f53867d3b42f8758f1d04e6ec801948a3d5f13a8
JiaJun <113363551+jiajunli188@users.noreply.github.com>
2023-04-26 13:21:56 +0200

    Update pathfile

--- Main/Airline/DataAccess/GetPathFile.cs
+++ Main/Airline/DataAccess/GetPathFile.cs
@@ -5,6 +5,7 @@
         string pathfile = "";
         
         string pathfileJiajun = "C:\\Users\\JiaJun\\Documents\\GitHub\\Project-B\\Main\\Airline\\DataSources";
+        string pathfileJiajunLaptop = "C:\\Users\\JiaJun Li\\Documents\\GitHub\\Project-B\\Main\\Airline\\DataSources";
         string patfileMarissa = "C:\\Users\\mbraa\\Development_Y1\\projectB\\_project_B Airline\\Project-B\\Main\\Airline\\DataSources";
         string pathfileSoufiane = "C:\\Users\\soufi\\OneDrive\\Git\\Project B\\Project-B\\Main\\Airline\\DataSources";
         string pathfileMarieClaire = "C:\\Users\\marie\\OneDrive - Hogeschool Rotterdam\\Semester 2\\Project B\\Project-B\\Main\\Airline\\DataSources";
@@ -13,6 +14,11 @@
         if (Directory.Exists(pathfileJiajun))
         {
             pathfile = pathfileJiajun;
+        }
+
+        else if (Directory.Exists(pathfileJiajunLaptop))
+        {
+            pathfile = pathfileJiajunLaptop;
         }
 
         else if (Directory.Exists(patfileMarissa))
--- Main/Airline/Program.cs
+++ Main/Airline/Program.cs
@@ -24,12 +24,11 @@ class Program
         Luggage.LuggageInfo(CalculateTotalCosts.tickets);*/
 
         // [Jiajun] Het selecteren van de Catering en de prijzen uitrekenen
-        /*Airplane boeing747 = new Airplane("BOEING747", "BO", 1, 4, 5, 6, 2);
+        Airplane boeing747 = new Airplane("BOEING747", "BO", 1, 4, 5, 6, 2);
         Destination frankfort = new Destination("GERMANY", "FRANKFURT", "(GER)", "FRANKFORTAIRPORT", 100, 3, "On Schedule");
-        Flight flightfrankfort = new Flight(1, "GER1", "Day", boeing747, DateTime.Now, DateTime.Now, frankfort, 100);
-        CateringForm.Catering(flightfrankfort);*/
+        Flight flightfrankfort = new Flight(1, "GER1", "Day", boeing747, DateTime.Now, DateTime.Now, frankfort, 100, 0);
+        CateringForm.Catering(flightfrankfort);
 
-        /*Total  € 55,-*/
-        Menu.StartScreen();
+        /*Menu.StartScreen();*/
     }
 }
