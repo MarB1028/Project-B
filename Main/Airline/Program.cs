69d9f77accd5da4314e32a879ac7d6cbdc0fa0bd
SoufianeB01 <soufianeboufarache@gmail.com>
2023-06-02 10:01:02 +0200

    update

--- Main/Airline/Program.cs
+++ Main/Airline/Program.cs
@@ -50,17 +50,8 @@
 
         // [Jiajun] Het selecteren van de Catering en de prijzen uitrekenen
         Menu.StartScreen();
-        Airplane boeing747 = new Airplane("BOEING747", "BO", 1, 4, 5, 6, 2);
-        Destination frankfort = new Destination("1", "GERMANY", "FRANKFURT", "(GER)", "FRANKFORTAIRPORT", 100, 3, "On Schedule");
-        Flight flightfrankfort = new Flight(1, "GER1", "Day", boeing747, DateTime.Now, DateTime.Now, frankfort, 100, 100);
-        List<BookTicket> books = new List<BookTicket>();
-        CateringForm.Catering(flightfrankfort, books);
         // CalculateStartPrice.ApplyDeals();
         // CalculateStartPrice.ApplyPriceRise();
         // List<Flight> flights = DataFlights.ReadFlightsFromJson();
-        
-
-
-        Menu.StartScreen();
     }
 }
